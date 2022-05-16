f6aa8899eef7fed987f47706d8854df1460778d7
kumar-tallapalli <77386924+kumar-tallapalli@users.noreply.github.com>
2022-05-16 23:21:37 +0530

    Update gtm_dynamic_load_script.js

--- gtm_dynamic_load_script.js
+++ gtm_dynamic_load_script.js
@@ -1,6 +1,3 @@
-document.onload = function(){
-
-	
 //QA OneTrust Cookies Consent Notice start for infineon.com
 const OtAutoBlockScript = document.createElement('script')
 OtAutoBlockScript.src = 'https://cdn.cookielaw.org/consent/85f8227d-e015-4c62-ab96-ee1a08760fb3-test/OtAutoBlock.js'
@@ -13,23 +10,22 @@ document.head.innerHTML = document.head.innerHTML+otSDKStubScript;
 //QA OneTrust Cookies Consent Notice end for infineon.com
 
 // Google Tag Manager
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N8N7WR8');
-// End Google Tag Manager
-	
-}
+const GTMSCRIPT_ID = "GTM-N8N7WR8";
+const GTMSCRIPT = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':"+
+"new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],"+
+"j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src="+
+"'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);"+
+"})(window,document,'script','dataLayer','"+GTMSCRIPT_ID+"');";
 
+document.head.innerHTML = document.head.innerHTML+GTMSCRIPT;
+
+// End Google Tag Manager
 
 window.onload = function() {
 	// Google Tag Manager (noscript)
-document.body.innerHTML = document.body.innerHTML + '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8N7WR8"'+
+document.body.innerHTML = document.body.innerHTML + '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id='+GTMSCRIPT_ID+'"'+
 				'height="0" width="0" style="display:none;visibility:hidden"><\/noscript>';
 // End Google Tag Manager (noscript)
 
 }
-
-
 
