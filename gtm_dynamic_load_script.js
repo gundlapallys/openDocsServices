c1086e59bb0cd478dbc595d24b9e3d6ed3ba4d64
kumar-tallapalli <77386924+kumar-tallapalli@users.noreply.github.com>
2022-05-16 20:55:35 +0530

    gtm script file

---
+++ gtm_dynamic_load_script.js
@@ -0,0 +1,28 @@
+//QA OneTrust Cookies Consent Notice start for infineon.com
+const OtAutoBlockScript = document.createElement('script')
+OtAutoBlockScript.src = 'https://cdn.cookielaw.org/consent/85f8227d-e015-4c62-ab96-ee1a08760fb3-test/OtAutoBlock.js'
+document.head.append(OtAutoBlockScript);
+
+const otSDKStubScript = document.createElement('script')
+otSDKStubScript.src = 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js'
+document.head.append(otSDKStubScript);
+
+function OptanonWrapper() { }
+//QA OneTrust Cookies Consent Notice end for infineon.com
+
+
+// Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N8N7WR8');
+// End Google Tag Manager
+
+
+// Google Tag Manager (noscript)
+var noscript = '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8N7WR8"'+
+				'height="0" width="0" style="display:none;visibility:hidden"><\/noscript>';
+document.body.write(noscript);
+	
+// End Google Tag Manager (noscript)
