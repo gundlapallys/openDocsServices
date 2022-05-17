e93b93f72283209baf9c1dab0f194f7ad389a909
kumar-tallapalli <77386924+kumar-tallapalli@users.noreply.github.com>
2022-05-17 11:17:29 +0530

    Update gtm_dynamic_load_script.js

--- gtm_dynamic_load_script.js
+++ gtm_dynamic_load_script.js
@@ -13,14 +13,15 @@ document.head.append(otSDKStubScript);
 
 // Google Tag Manager
 const GTMSCRIPT_ID = "GTM-N8N7WR8";
-const GTMSCRIPT = "<script type='text/javascript'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':"+
-"new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],"+
-"j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src="+
-"'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);"+
-"})(window,document,'script','dataLayer','"+GTMSCRIPT_ID+"');</script>";
-
-document.head.innerHTML = document.head.innerHTML+GTMSCRIPT;
-
+const GTMSCRIPT = document.createElement('script');
+GTMSCRIPT.onload = function(){
+	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer',GTMSCRIPT_ID);
+}
+document.head.append(GTMSCRIPT);
 // End Google Tag Manager
 
 window.onload = function() {
