378d1d67b669b01d0cb84f999c2e975c6711c507
kumar-tallapalli <77386924+kumar-tallapalli@users.noreply.github.com>
2022-05-17 10:10:47 +0530

    Update gtm_dynamic_load_script.js

--- gtm_dynamic_load_script.js
+++ gtm_dynamic_load_script.js
@@ -11,10 +11,10 @@ document.head.innerHTML = document.head.innerHTML+otSDKStubScript;
 
 // Google Tag Manager
 const GTMSCRIPT_ID = "GTM-N8N7WR8";
-const GTMSCRIPT = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':"+
+const GTMSCRIPT = "<script type='text/javascript'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':"+
 "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],"+
 "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src="+
-"'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);"+
+"'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);</script>"+
 "})(window,document,'script','dataLayer','"+GTMSCRIPT_ID+"');";
 
 document.head.innerHTML = document.head.innerHTML+GTMSCRIPT;
@@ -24,7 +24,7 @@ document.head.innerHTML = document.head.innerHTML+GTMSCRIPT;
 window.onload = function() {
 	// Google Tag Manager (noscript)
 document.body.innerHTML = document.body.innerHTML + '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id='+GTMSCRIPT_ID+'"'+
-				'height="0" width="0" style="display:none;visibility:hidden"><\/noscript>';
+				'height="0" width="0" style="display:none;visibility:hidden"></noscript>';
 // End Google Tag Manager (noscript)
 
 }
