c196197577bfb0324813118ccb5626e4f723423d
kumar-tallapalli <77386924+kumar-tallapalli@users.noreply.github.com>
2022-06-06 09:31:15 +0530

    production one trust changes
    production one trust changes

--- gtm_dynamic_load_script.js
+++ gtm_dynamic_load_script.js
@@ -1,13 +1,13 @@
 //QA OneTrust Cookies Consent Notice start for infineon.com
 const OtAutoBlockScript = document.createElement('script');
-OtAutoBlockScript.src = 'https://cdn.cookielaw.org/consent/85f8227d-e015-4c62-ab96-ee1a08760fb3-test/OtAutoBlock.js';
+OtAutoBlockScript.src = 'https://cdn.cookielaw.org/consent/85f8227d-e015-4c62-ab96-ee1a08760fb3/OtAutoBlock.js';
 document.head.append(OtAutoBlockScript);
 
 const otSDKStubScript = document.createElement('script');
 otSDKStubScript.src = 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js';
 otSDKStubScript.setAttribute('data-document-language','true');
 otSDKStubScript.setAttribute('charset','UTF-8');
-otSDKStubScript.setAttribute('data-domain-script','85f8227d-e015-4c62-ab96-ee1a08760fb3-test');
+otSDKStubScript.setAttribute('data-domain-script','85f8227d-e015-4c62-ab96-ee1a08760fb3');
 document.head.append(otSDKStubScript);
 //QA OneTrust Cookies Consent Notice end for infineon.com
 
