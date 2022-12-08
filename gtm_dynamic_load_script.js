966be6e06dd9d7198f922b5c08bfec97ed09bdc8
kumar-tallapalli <77386924+kumar-tallapalli@users.noreply.github.com>
2022-12-08 15:20:07 +0530

    Update gtm_dynamic_load_script.js

--- gtm_dynamic_load_script.js
+++ gtm_dynamic_load_script.js
@@ -19,12 +19,3 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer',GTMSCRIPT_ID);
 // End Google Tag Manager
-
-window.onload = function() {
-	// Google Tag Manager (noscript)
-document.body.innerHTML = document.body.innerHTML + "<noscript><iframe src='https://www.googletagmanager.com/ns.html?id="+GTMSCRIPT_ID+"'"+
-				"height='0' width='0' style='display:none;visibility:hidden'></noscript>";
-// End Google Tag Manager (noscript)
-
-}
-
